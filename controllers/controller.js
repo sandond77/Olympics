755d21777a00725c941f40e95f70c98ccd14b084
Josh Silverman <jrsilverman71@hotmail.com>
2018-03-01 20:13:09 -0800

    routing for node

--- controllers/controller.js
+++ controllers/controller.js
@@ -8,43 +8,64 @@ router.get('/', function(req, res){
 });
 
 router.get('/gold', function(req, res){
-  	db.Medalist.findAll({
-    	where: {
-     	 	medals: gold
-    	}
-  	}).then(function(data){
-    	console.log(data)
-    	//Need to figure out how and where to render our data an to the page...
-  	});
+  res.sendFile(path.join(__dirname, '../views/gold.html'));
 });
 
-
 router.get('/silver', function(req, res){
-  	db.Medalist.findAll({
-    	where: {
-      		medals: silver
-    	}
-  	}).then(function(data){
-   		console.log(data)
-  	});
+  res.sendFile(path.join(__dirname, '../views/silver.html'));
 });
 
 router.get('/bronze', function(req, res){
-  	db.Medalist.findAll({
-    	where: {
-      		medals: bronze
-    	}
-  	}).then(function(data){
-    	console.log(data)
-  	});
+  res.sendFile(path.join(__dirname, '../views/bronze.html'));
 });
 
-router.post('/add', function(req, res){
-	db.Medalist.create({
-		//insert data here
-  	}).then(function(data){
-   	 	console.log(data)
-  	});
+router.get('/comments', function(req, res){
+  res.sendFile(path.join(__dirname, '../views/comments.html'));
 });
+
+
+
+
+
+
+// router.get('/gold', function(req, res){
+//   	db.Medalist.findAll({
+//     	where: {
+//      	 	medals: gold
+//     	}
+//   	}).then(function(data){
+//     	console.log(data)
+//     	//Need to figure out how and where to render our data an to the page...
+//   	});
+// });
+
+
+// router.get('/silver', function(req, res){
+//   	db.Medalist.findAll({
+//     	where: {
+//       		medals: silver
+//     	}
+//   	}).then(function(data){
+//    		console.log(data)
+//   	});
+// });
+
+// router.get('/bronze', function(req, res){
+//   	db.Medalist.findAll({
+//     	where: {
+//       		medals: bronze
+//     	}
+//   	}).then(function(data){
+//     	console.log(data)
+//   	});
+// });
+
+// router.post('/add', function(req, res){
+// 	db.Medalist.create({
+// 		//insert data here
+//   	}).then(function(data){
+//    	 	console.log(data)
+//   	});
+// });
 
 module.exports = router;
